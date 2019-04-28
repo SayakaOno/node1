c9283214f0aaacdbf0bcaa2b1f1ce7e052fac914
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-04-27 21:43:07 -0700

    rewrite API request with jQuery

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,33 @@
-let button = document.querySelector('button');
 let element = document.getElementById('content');
 let term = document.body.querySelector('input[name="term"]');
 let defined = document.body.querySelector('input[name="defined"]');
 
-button.onclick = e => {
-  e.preventDefault();
-  let req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState === 4) {
-      if (req.status === 200) {
-        renderDictionary(JSON.parse(req.response));
-        term.value = '';
-        defined.value = '';
-      }
+$(function() {
+  $.ajax({
+    type: 'GET',
+    url: '/dictionary-api',
+    success: function(data) {
+      renderDictionary(data);
     }
+  });
+});
+
+$('button').on('click', function() {
+  var newData = {
+    term: term.value,
+    defined: defined.value
   };
-  req.open('POST', '/dictionary-api', true);
-  req.setRequestHeader(
-    'content-type',
-    'application/x-www-form-urlencoded;charset=UTF-8'
-  );
 
-  req.send(`term=${term.value}&defined=${defined.value}`);
-};
-
-async function getData() {
-  let response = await fetch('/dictionary-api');
-  let data = await response.json();
-  return data;
-}
-
-getData().then(data => {
-  renderDictionary(data);
+  $.ajax({
+    type: 'POST',
+    url: '/dictionary-api',
+    data: newData,
+    success: function(data) {
+      renderDictionary(data);
+      term.value = '';
+      defined.value = '';
+    }
+  });
 });
 
 function renderDictionary(data) {
