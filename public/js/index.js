406a06fce37d7c107d5c152aff4cafb7596cfb4c
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-04-28 10:39:19 -0700

    rewrite API request with Async/Await

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,58 @@
 let element = document.getElementById('content');
+let button = document.querySelector('button');
 let term = document.body.querySelector('input[name="term"]');
 let defined = document.body.querySelector('input[name="defined"]');
 
-$(function() {
-  $.ajax({
-    type: 'GET',
-    url: '/dictionary-api',
-    success: function(data) {
-      renderDictionary(data);
-    }
-  });
-});
+getData();
 
-$('button').on('click', function() {
-  var newData = {
+button.onclick = postData;
+
+async function getData() {
+  let response = await fetch('/dictionary-api');
+  let data = await response.json();
+  renderDictionary(data);
+}
+// jQuery //
+// $(function() {
+// $.ajax({
+//   type: 'GET',
+//   url: '/dictionary-api',
+//   success: function(data) {
+//     renderDictionary(data);
+//   }
+// });
+// });
+
+async function postData() {
+  let newData = {
     term: term.value,
     defined: defined.value
   };
-
-  $.ajax({
-    type: 'POST',
-    url: '/dictionary-api',
-    data: newData,
-    success: function(data) {
-      renderDictionary(data);
-      term.value = '';
-      defined.value = '';
-    }
+  let response = await fetch('/dictionary-api', {
+    method: 'POST',
+    body: JSON.stringify(newData),
+    headers: { 'Content-Type': 'application/json' }
   });
-});
+  let data = await response.json();
+  renderDictionary(data);
+  term.value = '';
+  defined.value = '';
+}
+// jQuery //
+// let newData = {
+//   term: term.value,
+//   defined: defined.value
+// };
+// $.ajax({
+//   type: 'POST',
+//   url: '/dictionary-api',
+//   data: newData,
+//   success: function(data) {
+//     renderDictionary(data);
+//     term.value = '';
+//     defined.value = '';
+//   }
+// });
 
 function renderDictionary(data) {
   let html = '';
